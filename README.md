e17ff16a8489965d9a89746236922e99c5435e63
yrribeiro <ynk9a@outlook.com>
2022-05-11 18:33:32 -0300

    project done + readme explanation

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # Solar System Prototype
-This is a work in progress.</br>
+This is a <i><b>solar system prototype made with OpenGL 3.3, GLUT and C++</b></i>
 
-<i>Come back later for a <b>solar system prototype made with GLEW, GLFW and OpenGL 3.3<b></i>
+The purpose of this project is to develop a program capable of representing a planet rotation movements. To do so, it were created a main celestial body where the other bodies will rotate around (aka The Sun), an inner and outer planet. In addition, the blue planet (outer) has two moons orbiting in different axis.
+
+By pressing <i>Y</i> in your keyboard, you can see them rotating.
+
+## Demo
+![screen-gif](./solar-system-running.gif)
 
 ## Compile and execute
 ```
-g++ main.cpp -o solar -std=c++11 -Wall -lGL -lGLU -lglut -lGLEW -lglfw -lX11 -lXxf86vm -lXrandr -lpthread -lXi -ldl -lXinerama -lXcursor
+g++ main.cpp -o solar -lGL -lGLU -lglut
 ```
 
 ```
--- main.cpp
+++ main.cpp
@@ -1,54 +1,150 @@
 #include<iostream>
-#include<GL/glew.h>
-#include<GLFW/glfw3.h>
+#include<GL/gl.h>
+#include<GL/glut.h>
+#include<GL/glu.h>
 
-const GLint WIDTH = 800, HEIGHT = 600;
-const char *WIN_TITLE = "Solar System Prototype";
+#define WIDTH 850
+#define HEIGHT 600
+#define X 1100
+#define Y 10
 
-int main(){
-    if (!glfwInit()){
-        printf("GLFW FAILED!!");
-        glfwTerminate();
-        return 1;
-    }
+typedef struct TRI_DIM_COORDINATES {
+    float x, y, z;
+}TRI_DIM_COORDINATES;
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // setting OPENGL versions: (MAJOR)3.3(MINOR)
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+static GLfloat orbitAngleInnerPlanet = 50.0f;
+static GLfloat orbitAngleOuterPlanet = 360.0f;
+static GLfloat orbitAngleMoonY = 0.0f;
+static GLfloat orbitAngleMoonX = 45.0f;
+static TRI_DIM_COORDINATES coordinates;
+static TRI_DIM_COORDINATES moonY;
+static TRI_DIM_COORDINATES moonX;
 
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // core profile = no backwards compatibility
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE); // yes to forward compatibility
+void displaySolarSystem();
+void drawSun();
+void drawInnerPlanet();
+void drawOuterPlanet();
+void drawMoonX();
+void drawMoonY();
+void handleKeyboardStroke(unsigned char key, int x, int y);
 
-    //@TODO modularize those initialization functions
+int main(int argc, char** argv){
+    const char *TITLE = "Solar System Prototype";
 
-    GLFWwindow *mainWindow = glfwCreateWindow(WIDTH, HEIGHT, WIN_TITLE, NULL, NULL);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(WIDTH, HEIGHT);
+	glutCreateWindow(TITLE);
+    glutPositionWindow(X, Y);
+    glutDisplayFunc(displaySolarSystem);
+    glutKeyboardFunc(handleKeyboardStroke);
 
-    if (!mainWindow){
-        printf("WINDOWN FAILED!!");
-        glfwTerminate();
-        return 1;
-    }
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
 
-    int ndv, ndh; // size of addressable points, vertically and horizontally
-    glfwGetFramebufferSize(mainWindow, &ndh, &ndv); // getting the recently created window frame buffer size
+    float FOV_Y = 45.0f;
+    gluPerspective(FOV_Y, 1.0f*WIDTH/HEIGHT, 0.1f, 1050.0f);
+    glEnable(GL_DEPTH_TEST);
 
-    glfwMakeContextCurrent(mainWindow); // set context for GLEW to use == saying which window I want my things to be drawn on
-    glewExperimental = GL_TRUE; // allow modern extension features
-
-    if (glewInit() != GLEW_OK){
-        printf("GLEW FAILED!!");
-        glfwDestroyWindow(mainWindow);
-        glfwTerminate();
-        return 1;
-    }
-
-    glViewport(0, 0, ndh, ndv); // setting the viewport size, making the whole window a drawable area
-
-    while(!glfwWindowShouldClose(mainWindow)){ // loop until window is closed
-        glfwPollEvents(); // event listener and handler
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // clear | 0-1 notation, just divide by 256
-        glClear(GL_COLOR_BUFFER_BIT); // what I want to clear in the buffer
-        glfwSwapBuffers(mainWindow); // didnt get it.. theres what user can see and what we can draw...
-    }
+    glutMainLoop();
 
     return 0;
+}
+
+void displaySolarSystem(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    GLdouble CAMERA_POS_X = 0.0f, CAMERA_POS_Y = 0.0f, CAMERA_POS_Z = 5.0f;
+    gluLookAt(
+        CAMERA_POS_X,
+        CAMERA_POS_Y,
+        CAMERA_POS_Z,
+        0.0f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f
+    );
+    drawSun();
+    drawInnerPlanet();
+    drawOuterPlanet();
+    glutSwapBuffers();
+}
+
+void drawSun(){
+    glTranslatef(0.0f, 0.0f, -300.0f);
+    glColor3ub(255, 198, 26);
+    glutSolidSphere(20.0f, 30, 30);
+}
+
+void drawInnerPlanet(){
+    glPushMatrix();
+
+    glRotatef(orbitAngleInnerPlanet, coordinates.x, coordinates.y, coordinates.z);
+    glTranslatef(90.0f, 0.0f, 0.0f);
+    glColor3ub(130, 72, 0);
+    glutSolidSphere(4.0f, 15, 15);
+
+    glPopMatrix();
+}
+
+void drawOuterPlanet(){
+    glRotatef(orbitAngleOuterPlanet, coordinates.x, coordinates.y, coordinates.z);
+    glTranslatef(-90.0f, 0.0f, 0.0f);
+    glColor3ub(51, 85, 255);
+    glutSolidSphere(9.0f, 15.5f, 15.5f);
+
+    drawMoonY();
+    drawMoonX();
+}
+
+void drawMoonY(){
+    glPushMatrix();
+	glRotatef(orbitAngleMoonY, moonY.x, moonY.y, moonY.z);
+	glTranslatef(20.0f, 0.0f, 1.0f);
+    glColor3ub(192, 192, 192);
+	glutSolidSphere(2, 200, 200);
+    glPopMatrix();
+}
+
+void drawMoonX(){
+    glPushMatrix();
+	glRotatef(orbitAngleMoonX, moonX.x, moonX.y, moonX.z);
+	glTranslatef(10.0f, 0.0f, 0.0f);
+    glColor3ub(192, 192, 192);
+	glutSolidSphere(1, 200, 200);
+    glPopMatrix();
+}
+
+void handleKeyboardStroke(unsigned char key, int x, int y){
+    key = tolower(key);
+    if (key == 'y'){
+        coordinates.x = 0.0f;
+        coordinates.y = 1.0f;
+        coordinates.z = 0.0f;
+        orbitAngleInnerPlanet += 1.0f;         // move INNER planet
+        if (orbitAngleInnerPlanet > 360.0f) orbitAngleInnerPlanet = 0.0f;
+
+
+        orbitAngleOuterPlanet -= 1.0f;        // move OUTER planet
+        if (orbitAngleOuterPlanet == 0) orbitAngleOuterPlanet = 360.0f;
+
+        moonY.x = 0;
+        moonY.y = 1;
+        moonY.z = 0;
+        orbitAngleMoonY += 5.0f;               // move moon in Y-AXIS
+        if (orbitAngleMoonY > 360.0f) orbitAngleMoonY = 0.0f;
+
+        moonX.x = 0;
+        moonX.y = 1;
+        moonX.z = 1;
+        orbitAngleMoonX +=5.0f;               // move moon in X-AXIS
+        if (orbitAngleMoonX > 360.0f) orbitAngleMoonX = 0.0f;
+
+    }
+
+    glutPostRedisplay();
 }
